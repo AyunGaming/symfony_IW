df9d46dcf982feed9764b75188b3885605cb16fd
AyunGaming <brian.verchere@gmail.com>
2025-01-13 17:12:15 +0100

    Création de l'entité "Bookings"

---
+++ src/Entity/Bookings.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BookingsRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: BookingsRepository::class)]
+class Bookings
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne(inversedBy: 'bookings')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?users $user = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $orderDate = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUser(): ?users
+    {
+        return $this->user;
+    }
+
+    public function setUser(?users $user): static
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function getOrderDate(): ?\DateTimeInterface
+    {
+        return $this->orderDate;
+    }
+
+    public function setOrderDate(\DateTimeInterface $orderDate): static
+    {
+        $this->orderDate = $orderDate;
+
+        return $this;
+    }
+}
