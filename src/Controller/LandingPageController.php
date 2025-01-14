e4ae4f77ced99b109e41d7cff7d243e44ca9899d
Solène Lambert <solenelambert@Ordinateur-portable-de-Solene.local>
2025-01-14 14:40:22 +0100

    Revert "new page agenda"
    This reverts commit c6e61a9952904ab9a0fe7b49a03c89fc3a8ca878.

--- src/Controller/LandingPageController.php
+++ src/Controller/LandingPageController.php
@@ -2,10 +2,9 @@
 
 namespace App\Controller;
 
-use App\Repository\EventRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class LandingPageController extends AbstractController
 {
@@ -14,16 +13,6 @@ class LandingPageController extends AbstractController
     {
         return $this->render('landing_page/index.html.twig', [
             'title' => 'Bienvenue chez MonBar',
-        ]);
-    }
-
-    #[Route('/', name: 'homepage')]
-    public function event(EventRepository $eventRepository): Response
-    {
-        $firstEvent = $eventRepository->findOneBy([], ['date' => 'ASC']); 
-
-        return $this->render('landing_page/index.html.twig', [
-            'firstEvent' => $firstEvent,
         ]);
     }
 
